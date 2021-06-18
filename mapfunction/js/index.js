6148d3210e4e1de5b625cc51f34ce47b1272a5f3
Dave Spina <dspina79@gmail.com>
2021-06-17 23:48:18 -0400

    Added additional examples of maps

--- mapfunction/js/index.js
+++ mapfunction/js/index.js
@@ -1,4 +1,20 @@
+const PI = 3.14159;
 const food_items = ['potatoes', 'carrots', 'bread', 'grapes'];
 const items = food_items.map((item) => `<li>${item}</li>`);
 
 console.log(items);
+
+
+
+doubleNum = (x) => x * 2;
+c = (r) => 2 * r * PI;
+a = (r) => r * r * PI;
+
+const nums = [1,2,3,4,5,6,7];
+const double_nums = nums.map((num) => doubleNum(num));
+const circumferences = nums.map((num) => c(num));
+const areas = nums.map((num) => a(num));
+console.log(double_nums);
+console.log(circumferences);
+console.log(areas);
+
