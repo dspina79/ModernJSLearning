4198c69440bc7571ea1fc3691386bf386815317e
Dave Spina <dspina79@gmail.com>
2021-06-20 18:07:27 -0400

    Added inheritance to classes.

--- classes/js/index.js
+++ classes/js/index.js
@@ -18,8 +18,30 @@ class Person {
     }
 }
 
+class Manager extends Person {
+    constructor(firstName, lastName, department) {
+        super(firstName, lastName, 0);
+        this.department = department;
+        this.numberManaging = 0;
+    }
+
+    manage(person) {
+        this.numberManaging++;
+    }
+
+    getElevatorPitch() {
+        return `Hi, I'm ${this.firstName} ${this.lastName} and I manage ${this.numberManaging} people in the ${this.department} department.`;
+    }
+}
+
 const person1 = new Person('John', 'Miller', 40);
 person1.firstName = 'Jonathan';
 person1.middleName = 'Jacob';
 person1.sing();
 console.log(person1.getFullName());
+const person2 = new Person('Janice', 'Weller', 30);
+
+const manager = new Manager('Helen', 'Ferguson', 'Information Technologies');
+manager.manage(person1);
+manager.manage(person2);
+console.log(manager.getElevatorPitch());
