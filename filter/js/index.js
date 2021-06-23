82d281f2f1c4589939d23ef4ad922e1cdcf70545
Dave Spina <dspina79@gmail.com>
2021-06-23 05:26:06 -0400

    Added basic prime function

--- filter/js/index.js
+++ filter/js/index.js
@@ -1,6 +1,22 @@
-const myNumbers = [1,2,3,4,5,6,7,8,9,123,1234,54,3,5,3425,34];
+const myNumbers = [1,2,3,4,5,6,7,8,9,123,1234,54,3,5,3425,34, 37];
 const myEvenNumbers = myNumbers.filter((num) => num % 2 === 0);
 
 
 console.log(myNumbers);
 console.log(myEvenNumbers);
+
+function checkPrime(num) {
+    if (num % 2 === 0) {
+        return false;
+    }
+    for (let i = 3; i < num; i+=2) {
+        if (num % i === 0) {
+            return false;
+        }
+    }
+
+    return true;
+};
+
+const myPrimes = myNumbers.filter((num) => checkPrime(num));
+console.log(myPrimes);
