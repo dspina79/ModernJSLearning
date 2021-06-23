713dbe5a423214d97631543651f65782dac6c1df
Dave Spina <dspina79@gmail.com>
2021-06-22 20:33:01 -0400

    Implemented singleton pattern

---
+++ singletons/js/index.js
@@ -0,0 +1,25 @@
+class Sentinel {
+    constructor() {
+        if (Sentinel.instance instanceof Sentinel) {
+            return Sentinel.instance;
+        }
+
+        this.stateInformation = {
+            counter: 0,
+            lastName: 'Dude'
+        };
+
+        //Object.freeze(this.stateInformation);
+        Object.freeze(this);
+        Sentinel.instance = this;
+    }
+
+    get(key) {
+        return this.stateInformation[key];
+    }
+
+    setNewLastName(lastName) {
+        this.stateInformation.lastName = lastName;
+        this.stateInformation.counter++;
+    }
+}
