a4caf2349152d9914b61b6d1707049f30d40d000
Dave Spina <dspina79@gmail.com>
2021-06-22 20:38:14 -0400

    Added filter function

---
+++ filter/js/index.js
@@ -0,0 +1,6 @@
+const myNumbers = [1,2,3,4,5,6,7,8,9,123,1234,54,3,5,3425,34];
+const myEvenNumbers = myNumbers.filter((num) => num % 2 === 0);
+
+
+console.log(myNumbers);
+console.log(myEvenNumbers);
