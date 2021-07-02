84c677902a4e120533f1fd22692a15fe68407b4c
Dave Spina <dspina79@gmail.com>
2021-07-01 21:31:32 -0400

    Added proxy on a class instance.

--- proxies/js/index.js
+++ proxies/js/index.js
@@ -9,6 +9,16 @@ var handler = {
     }
 }
 
+class Student {
+    constructor(firstName, lastName, major) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.major = major;
+    }
+}
+
+const stu = new Student('Dean', 'Smith', 'French');
+
 var simpleObject = {
     firstName: 'foo',
     lastName: 'bar'
@@ -18,3 +28,8 @@ var px = new Proxy(simpleObject, handler);
 px.firstName;
 px.lastName;
 px.age;
+
+var ps = new Proxy(stu, handler);
+ps.firstName;
+ps.age;
+ps.major;
